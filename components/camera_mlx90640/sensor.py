5c61f0053b1ae051a46417160d0e1dea7a0f8402
Stephen Oliver <steve@infincia.com>
2025-12-21 11:18:09 -0600

    Remove unused import

--- components/camera_mlx90640/sensor.py
+++ components/camera_mlx90640/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
-from esphome.components import sensor, text_sensor, i2c
+from esphome.components import sensor, i2c
 from esphome.const import (
     CONF_ID,
     CONF_MIN_TEMPERATURE,
